14a905fefd80d3e071e66a0044b0e60e56033dfa
18127044 <giahungnguyen999@gmail.com>
2021-03-27 11:52:26 +0700

    rename fragment transaction

--- app/src/main/java/com/hcmus/apum/MainActivity.java
+++ app/src/main/java/com/hcmus/apum/MainActivity.java
@@ -32,33 +32,33 @@ public class MainActivity extends AppCompatActivity {
         this.savedInstanceState = savedInstanceState;
 
         // Init GUI
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frame, overview);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.addToBackStack(null);
-        ft.commit();
+        FragmentTransaction ft_main = getSupportFragmentManager().beginTransaction();
+        ft_main.replace(R.id.frame, overview);
+        ft_main.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft_main.addToBackStack(null);
+        ft_main.commit();
 
         // Init controls
         navBar = (BottomNavigationView) findViewById(R.id.navBar);
         navBar.setOnNavigationItemSelectedListener(item -> {
-            FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+            FragmentTransaction ft_navBar = getSupportFragmentManager().beginTransaction();
             switch (item.getItemId()) {
                 case R.id.action_overview:
-                    ft1.replace(R.id.frame, overview);
+                    ft_navBar.replace(R.id.frame, overview);
                     break;
                 case R.id.action_albums:
-                    ft1.replace(R.id.frame, albums);
+                    ft_navBar.replace(R.id.frame, albums);
                     break;
                 case R.id.action_faces:
-                    ft1.replace(R.id.frame, faces);
+                    ft_navBar.replace(R.id.frame, faces);
                     break;
                 case R.id.action_favorite:
-                    ft1.replace(R.id.frame, favorite);
+                    ft_navBar.replace(R.id.frame, favorite);
                     break;
             }
-            ft1.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-            ft1.addToBackStack(null);
-            ft1.commit();
+            ft_navBar.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft_navBar.addToBackStack(null);
+            ft_navBar.commit();
             return true;
         });
     }
