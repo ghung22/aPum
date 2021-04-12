1157a37b5ab79e2f2ba096e0fc8b116c27b79258
fangdai <pq.dai225@gmail.com>
2021-04-12 23:31:33 +0700

    Fixed Toolbar, Editing Favorite

--- app/src/main/java/com/hcmus/apum/PreviewActivity.java
+++ app/src/main/java/com/hcmus/apum/PreviewActivity.java
@@ -3,27 +3,38 @@ package com.hcmus.apum;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.appbar.AppBarLayout;
+
 import java.io.File;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class PreviewActivity extends AppCompatActivity {
 
     // GUI controls
     Toolbar toolbar;
-
+    AppBarLayout appbar;
     // Elements
     ImageView imgPreview;
-
+    public ArrayList<String> images_fav;
+    ArrayList<String> thumbnails;
+    int pos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +48,8 @@ public class PreviewActivity extends AppCompatActivity {
         Intent mainPreview = getIntent();
         Bundle bundle = mainPreview.getExtras();
         String[] items = bundle.getStringArray("items");
-        ArrayList<String> thumbnails = bundle.getStringArrayList("thumbnails");
-        int pos = bundle.getInt("position");
+        thumbnails = bundle.getStringArrayList("thumbnails");
+        pos = bundle.getInt("position");
         File imgFile = new File(thumbnails.get(pos));
         if(imgFile.exists()){
             Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
@@ -48,15 +59,37 @@ public class PreviewActivity extends AppCompatActivity {
 
         }
 //        imgPreview.setImageResource(thumbnails.get(pos));
-
         // Init actionbar buttons
         toolbar = (Toolbar) findViewById(R.id.menu_preview);
         toolbar.inflateMenu(R.menu.menu_preview);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
+
         if (actionBar != null) {
             actionBar.setTitle(imgFile.getName());
             actionBar.setDisplayHomeAsUpEnabled(true);
+            Toast.makeText(this, "test", Toast.LENGTH_LONG).show();
         }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_preview, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_favorite:
+                break;
+            case R.id.action_info:
+                images_fav.add(thumbnails.get(pos));
+                Toast.makeText(this, "oke", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getContext(),img.toString(), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_wallpaper:
+                break;
+        }
+        return true;
     }
 }
