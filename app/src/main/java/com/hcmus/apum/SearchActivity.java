56e6f27814f175c500e91f9eac5a8d5b537e1088
nateclint <giahungnguyen999@gmail.com>
2021-04-26 20:25:58 +0700

    search albums results click event

--- app/src/main/java/com/hcmus/apum/SearchActivity.java
+++ app/src/main/java/com/hcmus/apum/SearchActivity.java
@@ -54,6 +54,20 @@ public class SearchActivity extends AppCompatActivity {
         results.setOnItemClickListener((adapterView, view, i, l) -> showPreview(i));
     }
 
+    private void showContent(int pos) {
+        String albumPath = mediaManager.getAlbums().get(pos);
+        ArrayList<String> container = mediaManager.getAlbumContent(albumPath);
+        albumPath = albumPath.substring(albumPath.lastIndexOf("/") + 1);
+
+        Intent searchContent = new Intent(context, ContentActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("caller", "search");
+        bundle.putString("album", albumPath);
+        bundle.putStringArrayList("container", container);
+        searchContent.putExtras(bundle);
+        startActivityForResult(searchContent, CONTENT_REQUEST_CODE);
+    }
+
     private void showPreview(int pos) {
         Intent searchPreview = new Intent(context, PreviewActivity.class);
         Bundle bundle = new Bundle();
@@ -61,5 +75,39 @@ public class SearchActivity extends AppCompatActivity {
         bundle.putInt("position", pos);
         searchPreview.putExtras(bundle);
         startActivityForResult(searchPreview, 97);
+    }
+
+    private void resultClicked(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (scope) {
+            case "overview":
+                showPreview(i);
+                break;
+            case "albums":
+                showContent(i);
+                break;
+            case "faces":
+                break;
+            case "favorite":
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishActivity(SEARCH_REQUEST_CODE);
     }
 }
