f3a57d26cfafd980420668c61f1c9fa5898cbfe2
nateclint <giahungnguyen999@gmail.com>
2021-05-10 00:27:03 +0700

    FacesFragment

--- app/src/main/java/com/hcmus/apum/FacesFragment.java
+++ app/src/main/java/com/hcmus/apum/FacesFragment.java
@@ -1,37 +1,68 @@
 package com.hcmus.apum;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.ViewCompat;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FacesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.util.ArrayList;
+
+import static com.hcmus.apum.MainActivity.ABOUT_REQUEST_CODE;
+import static com.hcmus.apum.MainActivity.CAMERA_REQUEST_CODE;
+import static com.hcmus.apum.MainActivity.CONTENT_REQUEST_CODE;
+import static com.hcmus.apum.MainActivity.PREVIEW_REQUEST_CODE;
+import static com.hcmus.apum.MainActivity.SEARCH_REQUEST_CODE;
+import static com.hcmus.apum.MainActivity.mediaManager;
+
 public class FacesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // GUI controls
+    private AppBarLayout appbar;
+    private CollapsingToolbarLayout collapsingToolbar;
+    private Toolbar toolbar;
+    private NestedScrollView scroll;
+    private GridView grid;
+    private GridAdapter adapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Search
+    private MenuItem searchItem;
+    private SearchView searchView;
+
+    // Data
+    private ArrayList<String> mediaList = new ArrayList<>();
 
     public FacesFragment() {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static FacesFragment newInstance() {
-        FacesFragment fragment = new FacesFragment();
+    public static OverviewFragment newInstance(ArrayList<String> mediaList) {
+        OverviewFragment fragment = new OverviewFragment();
         Bundle args = new Bundle();
+        args.putStringArrayList("mediaList", mediaList);
         fragment.setArguments(args);
         return fragment;
     }
@@ -39,16 +70,179 @@ public class FacesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faces, container, false);
+        View view = inflater.inflate(R.layout.fragment_faces, container, false);
+        ViewCompat.requestApplyInsets(view); // TODO: restore scroll state
+
+        // Init data
+        mediaList = getArguments().getStringArrayList("mediaList");
+
+        // Init controls
+        appbar = view.findViewById(R.id.appbar);
+        appbar.addOnOffsetChangedListener(this::menuRecolor);
+        collapsingToolbar = view.findViewById(R.id.collapsingToolbar);
+        scroll = view.findViewById(R.id.scroll);
+        adapter = new GridAdapter(getActivity(), mediaList);
+        grid = view.findViewById(R.id.grid);
+        grid.setEmptyView(view.findViewById(R.id.faces_no_faces));
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(this::showContent);
+
+        // Init actionbar buttons
+        toolbar = view.findViewById(R.id.menu_faces);
+        toolbar.inflateMenu(R.menu.menu_faces);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        menuAction(item);
+        return true;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_faces, menu);
+
+        // Get controls
+        searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) searchItem.getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // Search focused/unfocused events
+        searchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(final MenuItem item) {
+                return menuShow(menu, false);
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(final MenuItem item) {
+                return menuShow(menu, true);
+            }
+        });
+
+        // Search query events
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                ArrayList<String> results = mediaManager.search(query, "faces");
+                if (!results.isEmpty()) {
+                    showSearch(query, results);
+                } else {
+                    Toast.makeText(getContext(), getContext().getText(R.string.err_search_not_found), Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(@NonNull Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        // Init icons
+        searchView.setSubmitButtonEnabled(true);
+        ImageView submitIcon = searchView.findViewById(androidx.appcompat.R.id.search_go_btn);
+        submitIcon.setImageResource(R.drawable.ic_search);
+    }
+
+    private void showContent(AdapterView<?> parent, View view, int pos, long id) {
+        String faceName = mediaList.get(pos);
+        ArrayList<String> container = null; //mediaManager.getFaceContent(faceName);
+
+        Intent mainContent = new Intent(this.getContext(), ContentActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("caller", "faces");
+        bundle.putString("face", faceName);
+        bundle.putStringArrayList("container", container);
+        mainContent.putExtras(bundle);
+        startActivityForResult(mainContent, CONTENT_REQUEST_CODE);
+    }
+
+    private void showSearch(String query, ArrayList<String> results) {
+        Intent mainSearch = new Intent(this.getContext(), SearchActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("caller", "faces");
+        bundle.putString("query", query);
+        bundle.putStringArrayList("results", results);
+        bundle.putString("scope", "faces");
+        mainSearch.putExtras(bundle);
+        startActivityForResult(mainSearch, SEARCH_REQUEST_CODE);
+    }
+
+    private boolean menuAction(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_search:
+                searchItem.expandActionView();
+                searchView.requestFocus();
+                break;
+            case R.id.action_select:
+                break;
+            case R.id.action_zoom:
+                break;
+            case R.id.action_sort:
+                // TODO: Sort in Overview
+                break;
+            case R.id.action_regenerate:
+                break;
+            case R.id.action_ignore:
+                break;
+            case R.id.action_settings:
+                break;
+            case R.id.action_about:
+                Intent mainAbout = new Intent(this.getContext(), AboutActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("caller", "overview");
+                mainAbout.putExtras(bundle);
+                mainAbout.setFlags(0);
+                startActivityForResult(mainAbout, ABOUT_REQUEST_CODE);
+                break;
+            default:
+                Toast.makeText(getContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    private void menuRecolor(AppBarLayout appBarLayout, int verticalOffset) {
+        // Change icon to black/white depending on scroll state
+        Menu menu = toolbar.getMenu();
+        MenuItem add = menu.findItem(R.id.action_add), search = menu.findItem(R.id.action_search);
+        if ((collapsingToolbar.getHeight() + verticalOffset) < (collapsingToolbar.getScrimVisibleHeightTrigger())) {
+            toolbar.getOverflowIcon().setColorFilter(getContext().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+            add.getIcon().setColorFilter(getContext().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+            search.getIcon().setColorFilter(getContext().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+        } else {
+            toolbar.getOverflowIcon().setColorFilter(getContext().getColor(R.color.black), PorterDuff.Mode.SRC_IN);
+            add.getIcon().setColorFilter(getContext().getColor(R.color.black), PorterDuff.Mode.SRC_IN);
+            search.getIcon().setColorFilter(getContext().getColor(R.color.black), PorterDuff.Mode.SRC_IN);
+        }
+    }
+
+    private boolean menuShow(Menu menu, boolean show) {
+        for (int i = 0; i < menu.size(); ++i) {
+            MenuItem item = menu.getItem(i);
+            if (item != searchItem)
+                item.setVisible(show);
+        }
+        return true;
+    }
     }
 }
